9879fd316f22d4ef61eaf51ae87512c5711bca8a
PumpkinSeed <qwer.kocka@gmail.com>
2018-03-04 13:28:03 +0100

    Finish guessing game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -21,32 +21,42 @@ fn main() {
 
     // the println!() is a macro that prints a string to the screen
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    // let creates a new variable which is immutable by default 
-    // the let mut means it will be mutable
-    // String is a string type provided by the standard library
-    // ::new() it's an associated function
-    let mut guess = String::new();
+    // loop indicates an infinite loop, breaks in case of win
+    loop {
+        println!("Please input your guess.");
 
-    // ::stdin() calls an associated function in io prelude
-    // read_line takes a &mut String as paramter and returns an io::Result
-    // the Result has a expect method if the Result wrappes an error 
-    // then cause a panic! and it's cause the crash of the program
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        // let creates a new variable which is immutable by default 
+        // the let mut means it will be mutable
+        // String is a string type provided by the standard library
+        // ::new() it's an associated function
+        let mut guess = String::new();
 
-    // here we 'shadow' the guess variable, it's lets us re-use the variable
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        // ::stdin() calls an associated function in io prelude
+        // read_line takes a &mut String as paramter and returns an io::Result
+        // the Result has a expect method if the Result wrappes an error 
+        // then cause a panic! and it's cause the crash of the program
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // cmp method can be called on anything that can be compared
-    // takes a reference what we want to compare to
-    // we use match statement to determine exactly what kind of 
-    // Ordering it is, Ordering is an enum
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        // here we 'shadow' the guess variable, it's lets us re-use the variable
+        // Err(_) ignores the error just continue to the next iteration
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // cmp method can be called on anything that can be compared
+        // takes a reference what we want to compare to
+        // we use match statement to determine exactly what kind of 
+        // Ordering it is, Ordering is an enum
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
     }
 }
