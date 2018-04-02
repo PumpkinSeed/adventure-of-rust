35a55ede11c34dba23d83c50afe8b51ebed441d8
PumpkinSeed <qwer.kocka@gmail.com>
2018-04-02 22:31:23 +0200

    Add primitive types example

---
+++ primitive-types/src/main.rs
@@ -0,0 +1,12 @@
+#![allow(unused_variables)]
+fn main() {
+    bool_ex()
+}
+
+// Example of boolean primitive type
+fn bool_ex() {
+    let x = true;
+
+    let y: bool = false;
+    println!("{}", y);
+}
