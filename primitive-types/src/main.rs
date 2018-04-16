e6bd9a6a85e00e9728ef416b77c3b80026352788
PumpkinSeed <qwer.kocka@gmail.com>
2018-04-16 23:38:28 +0200

    Add more type

--- primitive-types/src/main.rs
+++ primitive-types/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused_variables)]
 fn main() {
-    bool_ex()
+    char_ex()
 }
 
 // Example of boolean primitive type
@@ -9,4 +9,36 @@ fn bool_ex() {
 
     let y: bool = false;
     println!("{}", y);
+}
+
+fn char_ex() {
+    // char in Rust represents a single unicode scalar value
+    let x = 'x';
+    let two_hearts = '💕';
+
+    // char isn't a single byte, but four
+    println!("{}", two_hearts)
+}
+
+fn numeric_ex() {
+    let x = 42; // `x` has type `i32`.
+
+    let y = 1.0; // `y` has type `f64`.
+
+    // i8 -> fixed-sized integer
+    // i16 -> fixed-sized integer
+    // i32 -> fixed-sized integer
+    // i64 -> fixed-sized integer
+    // u8 -> fixed-sized unsigned integer
+    // u16 -> fixed-sized unsigned integer
+    // u32 -> fixed-sized unsigned integer
+    // u64 -> fixed-sized unsigned integer
+    // isize -> variable-sized integer
+    // usize -> variable-sized unsigned integer
+    // f32 -> fixed-sized float
+    // f64 -> fixed-sized float
+}
+
+fn arrays_ex() {
+    
 }
