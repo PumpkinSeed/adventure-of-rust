bcad8124dc96fccc3abbb3cde7fb96e3d771bb4a
PumpkinSeed <qwer.kocka@gmail.com>
2018-07-14 00:43:36 +0200

    Finish variables

--- c3-1-variables-and-mutability/src/main.rs
+++ c3-1-variables-and-mutability/src/main.rs
@@ -1,6 +1,22 @@
+const MAX_POINTS: u32 = 100_000;
+
 fn main() {
+    // Constants
+    println!("The value of MAX_POINTS is: {}", MAX_POINTS);
+
+    // Mutable variable
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    // Variable shadowing into the same name
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("The value of y is: {}", y);
+    // By using let, we can perform a few transformations on a value 
+    // but have the variable be immutable after those transformations 
+    // have been completed.
+    // We can change the type of the value but reuse the same name.
 }
