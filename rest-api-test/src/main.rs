264cf9c38eec42e99aa8efc4eb2ce8d32cf4dbbb
PumpkinSeed <qwer.kocka@gmail.com>
2018-12-13 15:17:15 +0100

    Add first web api

---
+++ rest-api-test/src/main.rs
@@ -0,0 +1,40 @@
+extern crate iron;
+extern crate time;
+
+use iron::prelude::*;
+use iron::status;
+use iron::{typemap, AfterMiddleware, BeforeMiddleware};
+use time::precise_time_ns;
+
+struct ResponseTime;
+
+impl typemap::Key for ResponseTime {
+    type Value = u64;
+}
+
+impl BeforeMiddleware for ResponseTime {
+    fn before(&self, req: &mut Request) -> IronResult<()> {
+        req.extensions.insert::<ResponseTime>(precise_time_ns());
+        Ok(())
+    }
+}
+
+impl AfterMiddleware for ResponseTime {
+    fn after(&self, req: &mut Request, res: Response) -> IronResult<Response> {
+        let delta = precise_time_ns() - *req.extensions.get::<ResponseTime>().unwrap();
+        println!("Request took: {} ms", (delta as f64) / 1000000.0);
+        Ok(res)
+    }
+}
+
+fn hello_world(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello World\n")))
+}
+
+fn main() {
+    let mut chain = Chain::new(hello_world);
+    chain.link_before(ResponseTime);
+    chain.link_after(ResponseTime);
+    println!("On 3000");
+    Iron::new(chain).http("localhost:3000");
+}
