4954c914d4e263bebdae3e7aa5aa3dc98827ae3b
PumpkinSeed <qwer.kocka@gmail.com>
2018-12-17 22:10:27 +0100

    Add list display

---
+++ rust-by-example/list-display/src/main.rs
@@ -0,0 +1,25 @@
+use std::fmt;
+
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec = &self.0s;
+
+        write!(f, "[")?;
+
+        for (count, v) in vec.iter().enumerate() {
+            if count != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}: {}", count, v)?;
+        }
+
+        write!(f, "]")
+    }
+}
+
+fn main() {
+    let v = List(vec![1, 6, 4, 8, 3, 7]);
+    println!("{}", v);
+}
