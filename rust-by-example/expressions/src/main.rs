745633bce9b38b9d330f32a4f836d707581aad95
PumpkinSeed <qwer.kocka@gmail.com>
2019-01-02 23:18:15 +0100

    Add expressions

---
+++ rust-by-example/expressions/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    // variable binding
+    let x = 5;
+
+    // expression
+    x;
+    x + 1;
+    15;
+
+    let x = 5u32;
+    let y = {
+        let x_squared = x * x;
+        let x_cube = x_squared * x;
+
+        // This expression will be assigned to `y`
+        x_cube + x_squared + x
+    };
+
+    let z = {
+        // The semicolon suppresses this expression and `()` is assigned to `z`
+        2 * x;
+    };
+
+    println!("x is {:?}", x);
+    println!("y is {:?}", y);
+    println!("z is {:?}", z);
+}
