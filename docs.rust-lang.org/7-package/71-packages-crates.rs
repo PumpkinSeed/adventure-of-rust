bcb35136dbf3744b519dfecdec4b4edcc1cabd91
PumpkinSeed <qwer.kocka@gmail.com>
2020-02-21 08:51:44 +0100

    Add 71

---
+++ docs.rust-lang.org/7-package/71-packages-crates.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // module system:
+    // pacakges: a cargo feature that lets you build, test, and share crates
+    // crates: a tree of modules that procuces a library or exacutable
+    // modules and use: let you control the organization, scope and privacy of paths
+    // paths: a way of naming an item, such as struct, function, or module
+
+    // packages and crates
+    // a crate is a binary or a library
+    // a package is one or more crates that providing a set of functionalities, contains Cargo.toml
+
+    // creating new package
+    // cargo new my-project
+    
+}
