6ef95b2d6b9876592dc5b5dc69bc339be6b6736f
PumpkinSeed <qwer.kocka@gmail.com>
2020-03-10 21:31:07 +0100

    Add default traits

--- docs.rust-lang.org/10-traits.rs
+++ docs.rust-lang.org/10-traits.rs
@@ -1,3 +1,9 @@
+// NOTE TO generics: use traits in generic
+use std::fmt;
+fn p<T: fmt::Debug>(x: T) { // all type which implements the Debug trait
+    println!(":?", x);
+}
+
 // traits like interfaces with some differences
 // define shared behaviour in an abstract way
 
@@ -6,6 +12,7 @@ pub trait Summary {
     fn summarize(&self) -> String;
 }
 
+// ------------------------------
 // Implementing a Trait on a Type
 pub struct NewsArticle {
     pub headline: String,
@@ -14,7 +21,7 @@ pub struct NewsArticle {
     pub content: String,
 }
 
-impl Sumary for NewsArticle {
+impl Summary for NewsArticle {
     fn summarize(&self) -> String {
         format!("{}, {} by ({})", self.headline, self.author, self.location)
     }
@@ -33,7 +40,7 @@ impl Summary for Tweet {
     }
 }
 
-fn main() {
+fn print_tweet() {
     let tweet = Tweet{
         username: String::from("horse_ebooks"),
         content: String::from("of course, as you probably already know, people"),
@@ -43,3 +50,13 @@ fn main() {
 
     println!("1 new tweet: {}", tweet.summarize());
 }
+
+// use the trait from a library:
+// use aggregator::Summary;
+// restriction: we can implement a trait on a type only if either the trait or the type is local to our crate
+// so we can't implement external traits on external types
+// this restriction is part of a property of programs called coherence, and more specifically the orphan rule
+
+// -----------------------
+// Default Implementations
+
