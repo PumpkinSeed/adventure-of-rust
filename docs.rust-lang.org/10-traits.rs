849d3fc76ca612857c1141a83005f439ed20f6b4
PumpkinSeed <qwer.kocka@gmail.com>
2020-03-13 13:24:00 +0100

    Finish traits

--- docs.rust-lang.org/10-traits.rs
+++ docs.rust-lang.org/10-traits.rs
@@ -60,3 +60,94 @@ fn print_tweet() {
 // -----------------------
 // Default Implementations
 
+pub trait SummaryDef {
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
+}
+// it has a default implementation and can be overwrite
+// default ex:
+
+impl SummaryDef for NewsArticle{}
+
+// we can call all functions inside the trait what the trait defines
+
+pub trait SummaryCall {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+// --------------------
+// Traits as Parameters
+
+// all type implements the Summary behaviour
+pub fn notify(item: impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// trait bound syntax
+pub fn notify_b<T: Summary>(item: T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+use std::fmt::Display;
+// specifying multiple trait bounds
+pub fn notify_m(item: impl Summary + Display) {}
+pub fn notify_b_m<T: Summary + Display>(item: T) {}
+
+// clearer trait bounds with where clauses
+fn some_function<T, U>(t: T, u: U) -> i32
+    where T: Display + Clone,
+          U: Clone + Debug {
+}
+
+// -------------------------------------
+// Returning Types that implement Traits
+
+fn returns_summarizable() -> impl Summary {
+    let t = Tweet::new();
+    t.username = String::from("test username");
+    t
+}
+
+// -----------------------
+// Fixing largest function
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+// -----------------------------------------------------
+// Using Trait Bounds to conditionally implement methods
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self {x, y}
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
